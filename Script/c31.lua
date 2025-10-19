9419dc0fe22b550070bbbc81415980ccde56e2b8
FlaretheKitsune <Firefox519121206@gmail.com>
2025-10-19 14:07:12 -0500

    Refactor Alpha-Cyber Gear Driver special summon effects
    Updated the Alpha-Cyber Gear Driver script to include special summon effects from hand and GY when used as Synchro material.

--- Script/c31.lua
+++ Script/c31.lua
@@ -2,91 +2,88 @@
 --Scripted by YourName
 local s,id=2323
 function s.initial_effect(c)
-    --Special Summon
+    --Special Summon from hand
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
     e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_HAND)
     e1:SetCountLimit(1,id)
-    e1:SetCost(s.spcost)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
     
-    --Search
+    --Special Summon from GY when used as Synchro Material
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetProperty(EFFECT_FLAG_DELAY)
-    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetCode(EVENT_BE_MATERIAL)
     e2:SetCountLimit(1,id+1)
-    e2:SetCondition(s.thcon)
-    e2:SetTarget(s.thtg)
-    e2:SetOperation(s.thop)
+    e2:SetCondition(s.spcon)
+    e2:SetTarget(s.sptg2)
+    e2:SetOperation(s.spop2)
     c:RegisterEffect(e2)
 end
 
-function s.cfilter(c)
-    return c:IsSetCard(0x2323) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
-end
-
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
-    Duel.Remove(g,POS_FACEUP,REASON_COST)
+--Special Summon from hand
+function s.desfilter(c,tp)
+    return c:IsFaceup() and c:IsSetCard(0x2323) and c:IsAbleToGrave() and Duel.GetMZoneCount(tp,c)>0
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then 
-        if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return false end
-        return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
-    end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+        and Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_MZONE)
 end
 
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
-    if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-        --Check if we can summon another monster
-        if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-        local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
-        if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-            Duel.BreakEffect()
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-            local sg=g:Select(tp,1,1,nil)
-            if #sg>0 then
-                Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-            end
-        end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+    if #g>0 and Duel.Destroy(g,REASON_EFFECT)~=0 then
+        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
     end
 end
 
+--Special Summon from GY when used as Synchro Material
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return r==REASON_SYNCHRO and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+
 function s.spfilter(c,e,tp)
-    return c:IsSetCard(0x2323) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(id)
+    return c:IsSetCard(0x2323) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+        and not c:IsCode(id) and c:IsLocation(LOCATION_GRAVE)
 end
 
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) and e:GetHandler():IsReason(REASON_EFFECT)
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 
-function s.thfilter(c)
-    return c:IsSetCard(0x2323) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
-end
-
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-end
-
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
     if #g>0 then
-        Duel.SendtoHand(g,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
+        local tc=Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        if #tc>0 then
+            --Negate effects
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_DISABLE)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+            tc:RegisterEffect(e1)
+            local e2=Effect.CreateEffect(e:GetHandler())
+            e2:SetType(EFFECT_TYPE_SINGLE)
+            e2:SetCode(EFFECT_DISABLE_EFFECT)
+            e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+            tc:RegisterEffect(e2)
+        end
     end
 end
