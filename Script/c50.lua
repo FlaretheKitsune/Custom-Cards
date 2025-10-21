f0c4c3b1f51509fcd554e2ff02436c1e0a253240
FlaretheKitsune <Firefox519121206@gmail.com>
2025-10-21 07:27:23 -0500

    Refactor search effect for 'Hero' cards
    Updated the search effect to allow adding 'Fusion' Spell or any 'Hero' card from the Deck to hand upon summon. Improved the filter criteria for the search effect.

--- Script/c50.lua
+++ Script/c50.lua
@@ -2,19 +2,21 @@
 --Scripted by YourName
 local s,id=GetID()
 function s.initial_effect(c)
-	--Search effect
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
-	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e2)
+ -- Add "Fusion" Spell or any "Hero" card from Deck to hand when summoned
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_TRIGGER_O + EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_SUMMON_SUCCESS)
+    e2:SetCountLimit(1, id)
+    e2:SetTarget(s.thtg)
+    e2:SetOperation(s.thop)
+    c:RegisterEffect(e2)
+
+    local e3 = e2:Clone()
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e3)
 	
 	--Unaffected by opponent's card effects
 	local e3=Effect.CreateEffect(c)
@@ -26,30 +28,27 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 
---Search effect
-function s.filter(c)
-	return (c:IsSpell() or c:IsTrap()) and c:IsAbleToHand() and 
-		   (c:ListsCode(CARD_HERO) or c:ListsArchetype(0x8) or 
-		   (c:GetText() and c:GetText():find("HERO")))
+-- Add "Fusion" Spell or any "Hero" card from Deck to hand when summoned
+function s.thfilter(c)
+    return c:IsAbleToHand() and ((c:IsType(TYPE_SPELL) and c:IsSetCard(0x46)) or (c:IsType(TYPE_MONSTER + TYPE_SPELL + TYPE_TRAP) and c:IsSetCard(0x8)))
 end
 
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-		return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) 
-	end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function s.thtg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.thfilter, tp, LOCATION_DECK, 0, 1, nil)
+    end
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK)
 end
 
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+function s.thop(e, tp, eg, ep, ev, re, r, rp)
+    local g = Duel.SelectMatchingCard(tp, s.thfilter, tp, LOCATION_DECK, 0, 1, 1, nil)
+    if #g > 0 then
+        Duel.SendtoHand(g, nil, REASON_EFFECT)
+        Duel.ConfirmCards(1 - tp, g)
+    end
 end
-
 --Unaffected by opponent's card effects
 function s.efilter(e,te)
 	return te:GetOwnerPlayer()~=e:GetHandlerPlayer()
+
 end
